0f98069aca681c6196fe3158bbd47e381ab3d1f6
TheDrawingCoding-Gamer <bulbyvr@gmail.com>
2025-10-19 17:33:28 -0400

    alright, i got distracted

--- yaoigen/shared/src/main/scala/gay/menkissing/yaoigen/util/catsextras.scala
+++ yaoigen/shared/src/main/scala/gay/menkissing/yaoigen/util/catsextras.scala
@@ -11,6 +11,17 @@ object catsextras {
 
   private case class IsMap[F[_], T <: Tuple](value: Tuple.Map[T, F])
 
+  private inline def parTupledGenericInner[M[_], F[_], T <: Tuple](tuple: Tuple.Map[T, M])(using nep: NonEmptyParallel.Aux[M, F]): F[T] =
+    inline IsMap[M, T](tuple) match
+      case t: IsMap[M, _ *: EmptyTuple] => nep.apply.map(nep.parallel(t.value.head))(_ *: EmptyTuple)
+      case t: IsMap[M, _ *: _] =>
+        val head = nep.parallel(t.value.head)
+        val tail = parTupledGenericInner(t.value.tail)
+        nep.apply.map2(head, tail)(_ *: _)
+  
+  private inline def parTupledGeneric[M[_], F[_], T <: Tuple](tuple: Tuple.Map[T, M])(using nep: NonEmptyParallel.Aux[M, F]): M[T] =
+    nep.sequential(parTupledGenericInner(tuple))
+
   private inline def tupledGeneric[F[_], T <: Tuple](tuple: Tuple.Map[T, F])(using app: Apply[F]): F[T] =
     inline IsMap[F, T](tuple) match
       case t: IsMap[F, _ *: EmptyTuple] => app.map(t.value.head)(_ *: EmptyTuple)
@@ -28,4 +39,10 @@ object catsextras {
     
     inline def reverseApN[R](f: F[(H *: Tuple.InverseMap[T, F]) => R]): F[R] =
       tuple.tupledEx <**> f
+  
+  extension[H, T <: Tuple, M[_], F[_]](tuple: M[H] *: T)(using toMap: Tuple.IsMappedBy[M][T], nep: NonEmptyParallel[M])
+    inline def parTupledEx: M[H *: Tuple.InverseMap[T, M]] =
+      parTupledGeneric(tuple.head *: toMap(tuple.tail))
+    inline def parMapNEx[R](f: (H *: Tuple.InverseMap[T, M]) => R): M[R] =
+      nep.flatMap.map(tuple.parTupledEx)(f)
 }
